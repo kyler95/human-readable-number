eb3aeab994d6f9d182b6fa8b3a39fa38086b4baf
Sergey Dresvyanskikh <kyler17@yandex.ru>
2022-06-23 15:58:40 +0700

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function toReadable (number) {
-  
+  const arr1 = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+  const arr2 = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+  //const remainder1 = number % 10;
+
+  if (number < 20)
+    return arr1[number];
+  else if (number < 100)
+    return arr2[Math.floor(number / 10) - 2] + (number % 10 ? " " + arr1[number % 10] : "");
+  else if (number < 1000)
+    return arr1[Math.floor(number / 100)] + " hundred" + (number % 100 ? " " + toReadable(number % 100) : "")
 }
